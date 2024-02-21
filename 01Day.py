cfdd588a60dc1e2ad4b3110ce833cab871ab73cc
shashikant62 <shashikantborude449@gmail.com>
2024-02-21 16:55:11 +0530

    python day one

--- 01Day.py
+++ 01Day.py
@@ -1,5 +1,18 @@
-print("hi")
-print(5)
-print(5+2)
-
+print(2)
 #first comments
+#secon way
+"""print("hi")
+print(5)
+print(5+2)"""
+
+
+#DATA TYPES
+a=1
+b="harry"
+c=True
+d=None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
