807ba78d3d45e575e80ee43d3f47aca216a901a7
shashikant62 <shashikantborude449@gmail.com>
2024-02-25 09:18:53 +0530

    committed

--- 03Day.py
+++ 03Day.py
@@ -2,3 +2,14 @@
 a="1"
 b="2"
 print(type(int(a)+int(b)))
+
+#user input
+a=input('hi')
+# print(type(a))
+print(type(int(a)))
+
+#String
+str1="apple"
+str2="hello banana"
+print(str1)
+print(str2)
