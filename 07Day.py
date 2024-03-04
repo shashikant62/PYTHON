5fbd4e03032d45d5e8fdfd3eb6317f8c0df476d8
shashikant62 <shashikantborude449@gmail.com>
2024-03-04 08:56:41 +0530

    Day_06 And Day_07

---
+++ 07Day.py
@@ -0,0 +1,6 @@
+#Datatypes/object types
+#Numbers,String,List,tuple(),dictionary{},Sets,file,boolean,none,functons,modules,classes
+a=12
+mylist=[1,2,"chi"]
+mydict={'one':'chai','two':'rte'}
+mytup=(1,2,3)#immutable
