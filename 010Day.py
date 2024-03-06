c3327db32ec3b7c0f0f4e96633674b895404186b
= <=>
2024-03-06 08:55:03 +0530

    STRINGS

---
+++ 010Day.py
@@ -0,0 +1,17 @@
+#STRINGS
+chai="masala chai"
+print(chai)
+print(chai[4])
+num="0123456789"
+# print(num[:])
+# print(num[0:7:2])
+# print(num[0:9])
+# print(chai.lower())
+# print(chai.upper())
+
+str="a, b, c, d"
+print(str.split()) #space basic
+print(str.split(", "))
+
+
+#LIST /ARRAY
