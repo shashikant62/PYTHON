f8e2605c3e85b2316d62bbfda29acdc5595dc11c
= <=>
2024-06-02 12:56:45 +0530

    DAY-12

---
+++ 012Day.py
@@ -0,0 +1,15 @@
+#DATA TYPES
+# -Numbers
+# -String
+# -List=[1,2,4]
+# -Tuple:(1,2,3)
+# -dic:{1,3,4}
+# -set:{a,b,v}
+# -File:open(1.txt)
+# -Boolean:t,f
+# -None
+# -Functions
+
+#ADVANCE=
+#decorator,generator,iterator,metaprograming
+
