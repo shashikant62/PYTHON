41b5a2527ce1767b5c713f155947b48534920890
= <=>
2024-06-05 12:15:47 +0530

    IF Else

---
+++ 017Day.py
@@ -0,0 +1,11 @@
+#CONDITIONAL
+x=int(input("what is your age:"))
+if(x<13):
+    print("child")
+elif(13<=x<=19):
+    print("Teenager")
+elif(20<=x<=59):
+    print("adult")
+else:
+    print("Senior")
+
